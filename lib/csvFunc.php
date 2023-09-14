d975215e435fdb0a319cdac7bf760b4e191c5364
Andrew-Keller <112123995+Andrew-Keller@users.noreply.github.com>
2023-09-14 19:52:24 -0400

    edited csvFunc.php
    in the middle of troubleshooting the format, index is changed on my machine for testing

--- lib/csvFunc.php
+++ lib/csvFunc.php
@@ -1,1 +1,12 @@
+<?php
 
+function csvFiletoArray($csvFile){
+	$f = fopen($csvFile,"r");
+
+	while ($record = fgetcsv($f)){
+		print($record[1]);
+		/*foreach ($record as $feild => $key) {
+			print($feild);
+		}*/
+	}
+}

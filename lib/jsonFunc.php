5f76d437593a34552479b978f60c3b5963e9bb80
juliannatruitt <143114521+juliannatruitt@users.noreply.github.com>
2023-09-14 21:54:55 -0400

    Update jsonFunc.php

--- lib/jsonFunc.php
+++ lib/jsonFunc.php
@@ -1,1 +1,10 @@
+<?php
 
+function jsonFiletoArray($path){
+	$f = fopen($path, 'r');
+	$content = fread($f, filesize($path));
+	$json_array = json_decode($content, true);
+	return $json_array;
+}
+
+?>

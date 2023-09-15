13e929363c765a04bbc49da06ea3c4755239b6d9
juliannatruitt <143114521+juliannatruitt@users.noreply.github.com>
2023-09-15 10:49:54 -0400

    Update jsonFunc.php - forgot to close the file in the function

--- lib/jsonFunc.php
+++ lib/jsonFunc.php
@@ -4,7 +4,7 @@ function jsonFiletoArray($path){
 	$f = fopen($path, 'r');
 	$content = fread($f, filesize($path));
 	$json_array = json_decode($content, true);
+	fclose($f);
 	return $json_array;
 }
-
 ?>

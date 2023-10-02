d7001e3d6be22ae9c7e8ac70fa06165faf23c6b0
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-10-02 19:52:30 -0400

    Update index.php

--- admin/contacts/index.php
+++ admin/contacts/index.php
@@ -2,9 +2,8 @@
 require_once("contacts.php");
 ?>
 
-<?php foreach($contacts as $name) {
+<?php foreach($contacts as $name) { ?>
 	<h4><?=$name['name'].""?></h4>
 	<p><?=$name['email'].""?> <br> <?=$name['phone'].""?></p>
 	<a href="detail.php">View Profile</a>
-}
-?>
+<?php } ?>

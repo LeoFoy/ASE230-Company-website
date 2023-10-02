84d78fd1c0d7d106aef10f6ff4099c3c2a8989eb
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-10-02 19:52:09 -0400

    Update detail.php

--- admin/contacts/detail.php
+++ admin/contacts/detail.php
@@ -3,7 +3,8 @@ require_once("contacts.php");
 ?>
 
 <?php foreach($contacts as $name) { ?>
+	<img class="picture" src=<?=$name['picture'].""?> alt="">
 	<h4><?=$name['name'].""?></h4>
 	<p><?=$name['email'].""?> <br> <?=$name['phone'].""?></p>
-	<a href="detail.php">View Profile</a>
 <?php } ?>
+<a href="index.php">Back to home page</a>

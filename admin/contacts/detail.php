943615dc0e55efbe591587f83a19ffa88c3544da
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-10-02 20:17:41 -0400

    Update detail.php

--- admin/contacts/detail.php
+++ admin/contacts/detail.php
@@ -8,5 +8,5 @@ $name=$contacts[$_GET['name']];
 
 <img class="picture" src=<?=$name['picture'].""?> alt="">
 <h4><?=$name['name'].""?></h4>
-<p><?=$name['email'].""?> <br> <?=$name['phone'].""?></p>
+<p>Email: <?=$name['email'].""?> <br> Phone: <?=$name['phone'].""?></p>
 <a href="index.php">Back to home page</a>

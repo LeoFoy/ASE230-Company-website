2de168b0e0ab29f5ad2b2b5559314ea12bc1fadb
juliannatruitt <143114521+juliannatruitt@users.noreply.github.com>
2023-11-05 12:17:29 -0500

    Update index.php

--- admin/awards/index.php
+++ admin/awards/index.php
@@ -1,21 +1,7 @@
 <?php
-//created by Julianna Truitt
-require_once("awards.php");
-$array_csv = csvFiletoArray("../../data/awards.csv");
+require_once('../modifyData.php');
+ModifyData::displayCsvFile('../../data/awards.csv', 'awards');
 ?>
-<h1>Awards index:</h1>
-<a href="create.php">Create a new award</a>
-
-<?php
-$index=0;
-foreach($array_csv as $item){ ?>
-	<div>
-		<h2><?= $item[0].': '.$item[1]; ?></h2>
-		<a href="detail.php?index=<?= $index; ?>">See details</a>
-		<?php $index++; ?>
-		<hr />
-	</div>
-<?php } ?>
 	
 
 
